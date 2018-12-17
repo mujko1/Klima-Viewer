549493256f0c061f4bfbb874429253b806fa62bf
Unknown <mujko1@bfh.ch>
2018-12-17 20:57:27 +0100

    Add models

---
+++ climaViewer/models/Location.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let Location = new Schema({
+  id: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  zip: {
+    type: Number
+  },
+  geoLocation: {
+    type: String
+  },
+  addedDate: {
+    type: String
+  }
+},{
+    collection: 'location'
+});
+
+module.exports = mongoose.model('Location', Location);
---
+++ climaViewer/models/WeatherRecord.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for Business
+let WeatherRecord = new Schema({
+  id: {
+    type: String
+  },
+  temperature: {
+    type: Number
+  },
+  wind: {
+    type: Number
+  },
+  pressure: {
+    type: Number
+  },
+  precipitation: {
+    type: Number
+  },
+  date: {
+    type: String
+  }
+},{
+    collection: 'weatherRecord'
+});
+
+module.exports = mongoose.model('WeatherRecord', WeatherRecord);
